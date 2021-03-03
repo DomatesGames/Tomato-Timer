f8813fa2279184aac4e34a2755555aa649fbb328
Domates_Games <74550237+DomatesGames@users.noreply.github.com>
2021-03-03 20:42:58 +0200

    Create tomato-timer-en.py

---
+++ source-code/tomato-timer-en.py
@@ -0,0 +1,10 @@
+#Tomato Timer 1.0, see: Source Code from Github.
+author = "TomatoGames from Github"
+hour = int (input ("Hour?"))
+hour = hour * 60 * 60
+minute = int (input ("Minute ??"))
+minute = minute * 60
+second = int (input ("Second?"))
+second = second
+time.sleep (hour + minute + second)
+print ("Time done!")
