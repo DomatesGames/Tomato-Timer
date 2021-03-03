56f7e6c7809f072a333744236e228fbcb613b3df
Domates_Games <74550237+DomatesGames@users.noreply.github.com>
2021-03-03 20:41:59 +0200

    Update tomato-timer-tr.py

--- source-code/tomato-timer-tr.py
+++ source-code/tomato-timer-tr.py
@@ -1,6 +1,5 @@
   
 import time
-import vlc
 author = "DomatesGames from Github"
 saat = int(input("Kaç saat? "))
 saat = saat*60*60
@@ -10,5 +9,3 @@ saniye = int(input("Kaç saniye? "))
 saniye = saniye
 time.sleep(saat + dakika + saniye)
 print("Süre doldu!")
-p = vlc.MediaPlayer(r"C:\Users\W7\Desktop\ses.mp3")
-p.play()
